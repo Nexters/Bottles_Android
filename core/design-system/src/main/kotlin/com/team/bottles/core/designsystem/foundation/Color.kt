6939957b34898416a975f2617edfae0004a2b798
GunHyung Ham <gdreagon@naver.com>
2024-07-23 18:24:08 +0900

    [feat] : foundation 컬러 정의

---
+++ core/design-system/src/main/kotlin/com/team/bottles/core/designsystem/foundation/Color.kt
@@ -0,0 +1,29 @@
+package com.team.bottles.core.designsystem.foundation
+
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+
+internal val PrimaryPurple500 = Color(0xFF615EFA)
+internal val PrimaryPurple400 = Color(0xFF8489FC)
+internal val PrimaryPurple300 = Color(0xFFB5B8FF)
+internal val PrimaryPurple200 = Color(0xFFD1D3FF)
+internal val PrimaryPurple100 = Color(0xFFEAEDFF)
+
+internal val Neutral900 = Color(0xFF202020)
+internal val Neutral800 = Color(0xFF414141)
+internal val Neutral700 = Color(0xFF5F5F5F)
+internal val Neutral600 = Color(0xFF747474)
+internal val Neutral500 = Color(0xFF9E9E9E)
+internal val Neutral400 = Color(0xFFBCBCBC)
+internal val Neutral300 = Color(0xFFE0E0E0)
+internal val Neutral200 = Color(0xFFECECEC)
+internal val Neutral100 = Color(0xFFF5F5F5)
+internal val Neutral50 = Color(0xFFFBFBFB)
+
+internal val Black = Color(0xFF000000)
+internal val White = Color(0xFFFFFFFF)
+internal val Red = Color(0xFFF03E3E)
+
+internal val Gradient = Brush.linearGradient(
+    0.0f to Neutral50, 1.0f to Neutral50
+)
