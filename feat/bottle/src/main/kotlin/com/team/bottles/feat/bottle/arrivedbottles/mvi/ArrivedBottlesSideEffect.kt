796447016457d91f47514efbcf49c1a098c79e14
GunHyung Ham <gdreagon@naver.com>
2024-07-31 04:51:34 +0900

    [feat] : ArrivedBottlesRoute 생성

---
+++ feat/bottle/src/main/kotlin/com/team/bottles/feat/bottle/arrivedbottles/mvi/ArrivedBottlesSideEffect.kt
@@ -0,0 +1,9 @@
+package com.team.bottles.feat.bottle.arrivedbottles.mvi
+
+import com.team.bottles.core.common.UiSideEffect
+
+sealed interface ArrivedBottlesSideEffect : UiSideEffect {
+
+    data object NavigateToSandBeach : ArrivedBottlesSideEffect
+
+}
---
+++ feat/bottle/src/main/kotlin/com/team/bottles/feat/bottle/arrivedbottles/mvi/ArrivedBottlesUiState.kt
@@ -0,0 +1,7 @@
+package com.team.bottles.feat.bottle.arrivedbottles.mvi
+
+import com.team.bottles.core.common.UiState
+
+data class ArrivedBottlesUiState(
+    val text: String = ""
+) : UiState
