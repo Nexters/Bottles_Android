d55db157a50f135e7693c48388589c6be73562d8
GunHyung Ham <gdreagon@naver.com>
2024-07-31 04:48:53 +0900

    [feat] : Navigator 정의

---
+++ core/navigator/src/main/kotlin/MainNavigator.kt
@@ -0,0 +1,14 @@
+import kotlinx.serialization.Serializable
+
+sealed class MainNavigator {
+
+    @Serializable
+    data object SandBeach : MainNavigator()
+
+    @Serializable
+    data object BottlesBox : MainNavigator()
+
+    @Serializable
+    data object MyPage : MainNavigator()
+
+}
