15253f0bd8a55ba9d7f6c1af98911fd64e0b37e3
GunHyung Ham <gdreagon@naver.com>
2024-07-31 04:52:18 +0900

    [feat] : BottleNavigation 정의

---
+++ feat/bottle/src/main/kotlin/com/team/bottles/feat/bottle/navigation/BottleNavigation.kt
@@ -0,0 +1,33 @@
+package com.team.bottles.feat.bottle.navigation
+
+import BottleNavigator
+import MainNavigator
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.team.bottles.feat.bottle.BottleRoute
+import com.team.bottles.feat.bottle.arrivedbottles.ArrivedBottlesRoute
+import com.team.bottles.feat.bottle.bottlebox.BottleBoxRoute
+
+fun NavGraphBuilder.arrivedBottlesScreen(
+    navigateToSandBeach: () -> Unit
+) {
+    composable<BottleNavigator.ArrivedBottles> {
+        ArrivedBottlesRoute(navigateToSandBeach = navigateToSandBeach)
+    }
+}
+
+fun NavGraphBuilder.bottleBoxScreen(
+    navigateToBottle: () -> Unit
+) {
+    composable<MainNavigator.BottlesBox> {
+        BottleBoxRoute(navigateToBottle = navigateToBottle)
+    }
+}
+
+fun NavGraphBuilder.bottleScreen(
+    navigateToBottleBox: () -> Unit
+) {
+    composable<BottleNavigator.Bottle> {
+        BottleRoute(navigateToBottleBox = navigateToBottleBox)
+    }
+}
