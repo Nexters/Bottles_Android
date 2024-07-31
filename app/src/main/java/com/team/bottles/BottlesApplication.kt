0a7da6e943b0b16094e8b7d37943ad0c60fec804
GunHyung Ham <gdreagon@naver.com>
2024-07-31 17:19:10 +0900

    [feat] : Kako Sdk 초기화

--- app/src/main/java/com/team/bottles/BottlesApplication.kt
+++ app/src/main/java/com/team/bottles/BottlesApplication.kt
@@ -1,7 +1,20 @@
 package com.team.bottles
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class BottlesApplication: Application()
+class BottlesApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        initKakaoLogin()
+    }
+
+    private fun initKakaoLogin() {
+        KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
+    }
+
+}
