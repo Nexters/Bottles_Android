1a0ee690f51e79181f71e426ebe9c1e1f3ac0ba4
GunHyung Ham <gdreagon@naver.com>
2024-08-02 04:22:29 +0900

    [feat] : Refresh Token을 위한 Authenticator 구현 맟 인증 헤더 필요 유무에 따른 OkHttpClient, Retrofit DI 구현

---
+++ core/network/src/main/kotlin/com/team/bottles/network/interceptor/AuthAuthenticator.kt
@@ -0,0 +1,47 @@
+package com.team.bottles.network.interceptor
+
+import com.team.bottles.core.datastore.datasource.TokenDataSource
+import com.team.bottles.network.datasource.AuthDataSource
+import kotlinx.coroutines.runBlocking
+import okhttp3.Authenticator
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.Route
+import javax.inject.Inject
+
+internal class AuthAuthenticator @Inject constructor(
+    private val tokenDataSource: TokenDataSource,
+    private val authDataSource: AuthDataSource,
+) : Authenticator {
+
+    override fun authenticate(route: Route?, response: Response): Request {
+        val newAccessToken = runBlocking { getNewAccessToken() }
+
+        return response.request.newBuilder()
+            .header(HEADER_NAME, "$TOKEN_TYPE $newAccessToken")
+            .build()
+    }
+
+    private suspend fun getNewAccessToken(): String? {
+        val currentRefreshToken = tokenDataSource.getRefreshToken()
+        val response = authDataSource.refreshAccessToken(refreshToken = currentRefreshToken).getOrNull()
+
+        if (response != null) {
+            tokenDataSource.setAccessToken(accessToken = response.accessToken)
+            tokenDataSource.setRefreshToken(refreshToken = response.refreshToken)
+
+            return response.accessToken
+        } else {
+            val accessToken = tokenDataSource.getAccessToken()
+            authDataSource.logout(accessToken = accessToken)
+            tokenDataSource.clear()
+            return null
+        }
+    }
+
+    companion object {
+        private const val HEADER_NAME = "Authorization"
+        private const val TOKEN_TYPE = "Bearer"
+    }
+
+}
