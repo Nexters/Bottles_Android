3b176eacc8dc300ce43c6665383e7b6ae8bf4bfa
GunHyung Ham <gdreagon@naver.com>
2024-08-05 23:51:23 +0900

    [refactor] : TopBar padding 값 변경

--- core/design-system/src/main/kotlin/com/team/bottles/core/designsystem/components/bars/TopBar.kt
+++ core/design-system/src/main/kotlin/com/team/bottles/core/designsystem/components/bars/TopBar.kt
@@ -9,18 +9,16 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.team.bottles.core.designsystem.R
 import com.team.bottles.core.designsystem.theme.BottlesTheme
 
 @Composable
@@ -32,14 +30,15 @@ fun BottlesTopBar(
     trailingIcon: (@Composable () -> Unit)? = null,
 ) {
     Box(
-        modifier = Modifier.padding(16.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(48.dp)
+            .background(backgroundColor)
+            .padding(horizontal =  16.dp),
         contentAlignment = Alignment.Center,
     ) {
         Row(
-            modifier = modifier
-                .fillMaxWidth()
-                .height(48.dp)
-                .background(backgroundColor),
+            modifier = modifier.fillMaxWidth(),
             horizontalArrangement = if (leadingIcon == null) Arrangement.End else Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
@@ -62,65 +61,75 @@ fun BottlesTopBar(
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = false)
 @Composable
 fun PreviewTopBar() {
-    Column {
-        // Preview 1: Leading Icon만 있는 경우
-        BottlesTopBar(
-            leadingIcon = {
-                IconButton(onClick = { /* Handle click */ }) {
-                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Back")
+    BottlesTheme {
+        Column {
+            // Preview 1: Leading Icon만 있는 경우
+            BottlesTopBar(
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_arrow_left_24),
+                        contentDescription = "Back"
+                    )
                 }
-            }
-        )
+            )
 
-        Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(16.dp))
 
-        // Preview 2: Leading Icon과 Trailing Icon만 있는 경우
-        BottlesTopBar(
-            leadingIcon = {
-                IconButton(onClick = { /* Handle click */ }) {
-                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Back")
+            // Preview 2: Leading Icon과 Trailing Icon만 있는 경우
+            BottlesTopBar(
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_arrow_left_24),
+                        contentDescription = "Back"
+                    )
+                },
+                trailingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_arrow_left_24),
+                        contentDescription = "Back"
+                    )
                 }
-            },
-            trailingIcon = {
-                IconButton(onClick = { /* Handle click */ }) {
-                    Icon(imageVector = Icons.Filled.ArrowForward, contentDescription = "Forward")
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Preview 3: Leading Icon과 Text가 있는 경우
+            BottlesTopBar(
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_arrow_left_24),
+                        contentDescription = "Back"
+                    )
+                },
+                text = "Title"
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Preview 4: Leading Icon, Text, Trailing Icon 모두 있는 경우
+            BottlesTopBar(
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_arrow_left_24),
+                        contentDescription = "Back"
+                    )
+                },
+                text = "Title",
+                trailingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_arrow_left_24),
+                        contentDescription = "Back"
+                    )
                 }
-            }
-        )
+            )
 
-        Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(16.dp))
 
-        // Preview 3: Leading Icon과 Text가 있는 경우
-        BottlesTopBar(
-            leadingIcon = {
-                IconButton(onClick = { /* Handle click */ }) {
-                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Back")
-                }
-            },
-            text = "Title"
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        // Preview 4: Leading Icon, Text, Trailing Icon 모두 있는 경우
-        BottlesTopBar(
-            leadingIcon = {
-                IconButton(onClick = { /* Handle click */ }) {
-                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Back")
-                }
-            },
-            text = "Title",
-            trailingIcon = {
-                IconButton(onClick = { /* Handle click */ }) {
-                    Icon(imageVector = Icons.Filled.ArrowForward, contentDescription = "Forward")
-                }
-            }
-        )
-
-        // Preview 5: 아무것도 없는 경우
-        BottlesTopBar()
+            // Preview 5: 아무것도 없는 경우
+            BottlesTopBar()
+        }
     }
 }
