078c6aff3db1b83cc3219969697bfb9fed1a9f1c
GunHyung Ham <gdreagon@naver.com>
2024-08-05 23:52:21 +0900

    [feat] : 자기소개 화면 구현

--- feat/profile/src/main/kotlin/com/team/bottles/feat/profile/introduction/IntroductionScreen.kt
+++ feat/profile/src/main/kotlin/com/team/bottles/feat/profile/introduction/IntroductionScreen.kt
@@ -1,18 +1,44 @@
 package com.team.bottles.feat.profile.introduction
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.team.bottles.core.designsystem.components.buttons.BottlesSolidButton
-import com.team.bottles.core.designsystem.components.buttons.SolidButtonType
-import com.team.bottles.feat.profile.createprofile.mvi.CreateProfileIntent
-import com.team.bottles.feat.profile.createprofile.mvi.CreateProfileUiState
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.team.bottles.core.designsystem.R
+import com.team.bottles.core.designsystem.components.bars.BottlesBottomBar
+import com.team.bottles.core.designsystem.components.bars.BottlesTopBar
+import com.team.bottles.core.designsystem.components.textfield.BottlesLinesMaxLengthTextField
+import com.team.bottles.core.designsystem.components.textfield.BottlesTextFieldState
+import com.team.bottles.core.designsystem.theme.BottlesTheme
+import com.team.bottles.core.ui.CardProfile
+import com.team.bottles.core.ui.model.UserKeyPoint
+import com.team.bottles.feat.profile.introduction.component.SelectImageCard
+import com.team.bottles.feat.profile.introduction.component.Title
 import com.team.bottles.feat.profile.introduction.mvi.IntroductionIntent
+import com.team.bottles.feat.profile.introduction.mvi.IntroductionStep
 import com.team.bottles.feat.profile.introduction.mvi.IntroductionUiState
 
 @Composable
@@ -20,17 +46,137 @@ internal fun IntroductionScreen(
     uiState: IntroductionUiState,
     onIntent: (IntroductionIntent) -> Unit
 ) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(text = "자소개 작성 화면")
-        BottlesSolidButton(
-            buttonType = SolidButtonType.XS,
-            text = "뒤로 가기",
-            onClick = { onIntent(IntroductionIntent.ClickBackButton) },
-            contentHorizontalPadding = 12.dp
+    val focusManager = LocalFocusManager.current
+    val interactionSource = remember { MutableInteractionSource() }
+    val isFocused by interactionSource.collectIsFocusedAsState()
+    val scrollState = rememberScrollState()
+
+    BackHandler {
+        onIntent(IntroductionIntent.ClickBackButton)
+    }
+
+    LaunchedEffect(isFocused) {
+        onIntent(IntroductionIntent.OnFocusedTextField)
+    }
+
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = BottlesTheme.color.background.primary)
+            .pointerInput(Unit) {
+                detectTapGestures(onTap = {
+                    focusManager.clearFocus()
+                })
+            },
+        topBar = {
+            BottlesTopBar(
+                leadingIcon = {
+                    Icon(
+                        modifier = Modifier.clickable(
+                            onClick = { onIntent(IntroductionIntent.ClickBackButton) }
+                        ),
+                        painter = painterResource(id = R.drawable.ic_arrow_left_24),
+                        contentDescription = null,
+                        tint = BottlesTheme.color.icon.primary
+                    )
+                }
+            )
+        },
+        bottomBar = {
+            BottlesBottomBar(
+                text = uiState.step.buttonText,
+                onClick = { onIntent(IntroductionIntent.ClickBottomButton) },
+                enabled = uiState.isEnabledWithBottomButton
+            )
+        }
+    ) { innerPadding ->
+        Box(modifier = Modifier.padding(innerPadding)) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = BottlesTheme.color.background.primary)
+                    .padding(horizontal = BottlesTheme.spacing.spacing16)
+                    .verticalScroll(state = scrollState),
+            ) {
+                Spacer(modifier = Modifier.height(height = BottlesTheme.spacing.spacing24))
+
+                Title(
+                    currentPage = uiState.step.page,
+                    maxPage = IntroductionStep.entries.size,
+                    title = uiState.step.title,
+                    subTitle = uiState.step.subTitle
+                )
+
+                Spacer(modifier = Modifier.height(height = BottlesTheme.spacing.spacing32))
+
+                when (uiState.step) {
+                    IntroductionStep.INPUT_INTRODUCTION -> {
+                        BottlesLinesMaxLengthTextField(
+                            value = uiState.introduce,
+                            onValueChange = { text ->
+                                onIntent(IntroductionIntent.ChangeIntroduction(text = text))
+                            },
+                            hint = stringResource(id = R.string.introduction_hint),
+                            maxLength = uiState.maxLength,
+                            state = uiState.introductionTextFiledState,
+                            interactionSource = interactionSource
+                        )
+
+                        if (uiState.introductionTextFiledState is BottlesTextFieldState.Error) {
+                            Spacer(modifier = Modifier.height(height = BottlesTheme.spacing.spacing4))
+
+                            Text(
+                                text = "최소 ${uiState.minLength}글자 이상 작성해주세요",
+                                style = BottlesTheme.typography.caption,
+                                color = BottlesTheme.color.text.errorPrimary
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.height(height = BottlesTheme.spacing.spacing12))
+
+                        CardProfile(keyPoints = uiState.keyPoints)
+                    }
+
+                    IntroductionStep.SELECT_USER_IMAGE -> {
+                        SelectImageCard(
+                            imageUri = uiState.imageUri,
+                            onIntent = onIntent
+                        )
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(height = BottlesTheme.spacing.spacing24))
+            }
+        }
+    }
+}
+
+@Preview(heightDp = 1100)
+@Composable
+private fun IntroductionScreenStep1Preview() {
+    BottlesTheme {
+        IntroductionScreen(
+            uiState = IntroductionUiState(
+                step = IntroductionStep.INPUT_INTRODUCTION,
+                keyPoints = UserKeyPoint.exampleUerKeyPoints(),
+            ),
+            onIntent = {}
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun IntroductionScreenStep2Preview() {
+    BottlesTheme {
+        IntroductionScreen(
+            uiState = IntroductionUiState(
+                step = IntroductionStep.SELECT_USER_IMAGE,
+                keyPoints = UserKeyPoint.exampleUerKeyPoints(),
+                isEnabledWithBottomButton = true,
+                imageUri = null
+            ),
+            onIntent = {}
         )
     }
 }
