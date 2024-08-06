5b8eace9cd4900188f951104805b0de9da4885a7
GunHyung Ham <gdreagon@naver.com>
2024-08-06 19:49:20 +0900

    [feat] : ProfileDataSource - postProfileImage 구현

--- core/network/src/main/kotlin/com/team/bottles/network/datasource/ProfileDataSource.kt
+++ core/network/src/main/kotlin/com/team/bottles/network/datasource/ProfileDataSource.kt
@@ -1,11 +1,12 @@
 package com.team.bottles.network.datasource
 
 import com.team.bottles.network.dto.profile.request.RegisterIntroductionRequest
+import java.io.File
 
 interface ProfileDataSource {
 
-    suspend fun createIntroduction(
-        request: RegisterIntroductionRequest
-    )
+    suspend fun createIntroduction(request: RegisterIntroductionRequest)
+
+    suspend fun postProfileImage(imageFile: File)
 
 }
--- core/network/src/main/kotlin/com/team/bottles/network/datasource/ProfileDataSourceImpl.kt
+++ core/network/src/main/kotlin/com/team/bottles/network/datasource/ProfileDataSourceImpl.kt
@@ -2,6 +2,10 @@ package com.team.bottles.network.datasource
 
 import com.team.bottles.network.api.ProfileService
 import com.team.bottles.network.dto.profile.request.RegisterIntroductionRequest
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import java.io.File
 import javax.inject.Inject
 
 class ProfileDataSourceImpl @Inject constructor(
@@ -10,6 +14,16 @@ class ProfileDataSourceImpl @Inject constructor(
 
     override suspend fun createIntroduction(request: RegisterIntroductionRequest) {
         profileService.createIntroduction(registerIntroductionRequest = request)
+    }
+
+    override suspend fun postProfileImage(imageFile: File) {
+        profileService.postProfileImage(
+            file = MultipartBody.Part.createFormData(
+                name = "file",
+                filename = imageFile.name,
+                body = imageFile.asRequestBody("image/*".toMediaType())
+            )
+        )
     }
 
 }
