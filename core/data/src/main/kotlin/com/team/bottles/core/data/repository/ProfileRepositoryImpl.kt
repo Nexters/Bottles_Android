de3e91dd1dc7e845764bb089eef6e570d96f7bdf
GunHyung Ham <gdreagon@naver.com>
2024-08-06 19:50:05 +0900

    [feat] : ProfileRepository 구현

--- core/data/src/main/kotlin/com/team/bottles/core/data/repository/ProfileRepositoryImpl.kt
+++ core/data/src/main/kotlin/com/team/bottles/core/data/repository/ProfileRepositoryImpl.kt
@@ -4,6 +4,7 @@ import com.team.bottles.core.data.mapper.toRequest
 import com.team.bottles.core.domain.profile.model.QuestionAndAnswer
 import com.team.bottles.core.domain.profile.repository.ProfileRepository
 import com.team.bottles.network.datasource.ProfileDataSource
+import java.io.File
 import javax.inject.Inject
 
 class ProfileRepositoryImpl @Inject constructor(
@@ -12,6 +13,10 @@ class ProfileRepositoryImpl @Inject constructor(
 
     override suspend fun createIntroduction(questionsAndAnswers: List<QuestionAndAnswer>) {
         profileDataSource.createIntroduction(request = questionsAndAnswers.toRequest())
+    }
+
+    override suspend fun uploadProfileImage(imageFile: File) {
+        profileDataSource.postProfileImage(imageFile = imageFile)
     }
 
 }
