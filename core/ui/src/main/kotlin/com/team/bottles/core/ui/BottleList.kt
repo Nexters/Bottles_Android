a861c91e6a13fc6276a07126287da4d1db17a5bf
GunHyung Ham <gdreagon@naver.com>
2024-08-08 21:51:20 +0900

    [feat] : BottleList 및 EmptyBottles UI 구현

---
+++ core/ui/src/main/kotlin/com/team/bottles/core/ui/BottleList.kt
@@ -0,0 +1,76 @@
+package com.team.bottles.core.ui
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.tooling.preview.Preview
+import com.team.bottles.core.designsystem.theme.BottlesTheme
+import com.team.bottles.core.ui.model.Bottle
+import kotlinx.collections.immutable.ImmutableList
+
+@Composable
+fun BottleContents(
+    bottles: ImmutableList<Bottle>,
+    emptyText: String,
+    @DrawableRes emptyImage: Int,
+    onClickItem: (Bottle) -> Unit
+) {
+    if (bottles.isEmpty()) {
+        EmptyBottles(
+            text = emptyText,
+            image = emptyImage
+        )
+    } else {
+        BottleList(
+            bottles = bottles,
+            onClickItem = onClickItem
+        )
+    }
+}
+
+@Composable
+fun BottleList(
+    modifier: Modifier = Modifier,
+    bottles: List<Bottle>,
+    onClickItem: (Bottle) -> Unit
+) {
+    LazyColumn(
+        modifier = modifier.fillMaxSize(),
+        contentPadding = PaddingValues(
+            top = BottlesTheme.spacing.extraLarge,
+            bottom = BottlesTheme.spacing.large,
+            start = BottlesTheme.spacing.medium,
+            end = BottlesTheme.spacing.medium
+        ),
+        verticalArrangement = Arrangement.spacedBy(
+            space = BottlesTheme.spacing.medium
+        )
+    ) {
+        items(
+            items = bottles,
+            key = { bottle -> bottle.id }
+        ) { bottle ->
+            BottleItem(
+                bottle = bottle,
+                onClickItem = { onClickItem(bottle) }
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun BottleListPreview() {
+    BottlesTheme {
+        BottleList(
+            bottles = Bottle.exampleBottleBox(),
+            onClickItem = {}
+        )
+    }
+}
