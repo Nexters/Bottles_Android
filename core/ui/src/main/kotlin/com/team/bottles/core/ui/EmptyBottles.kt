cf4334595785564752662d986489732e7350b7b8
GunHyung Ham <gdreagon@naver.com>
2024-08-17 04:53:29 +0900

    [refactor] : EmptyBottles 이미지 컴포저블 함수 변경

--- core/ui/src/main/kotlin/com/team/bottles/core/ui/EmptyBottles.kt
+++ core/ui/src/main/kotlin/com/team/bottles/core/ui/EmptyBottles.kt
@@ -1,24 +1,21 @@
 package com.team.bottles.core.ui
 
 import androidx.annotation.DrawableRes
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.skydoves.landscapist.coil.CoilImage
 import com.team.bottles.core.designsystem.R
 import com.team.bottles.core.designsystem.theme.BottlesTheme
 
@@ -42,15 +39,15 @@ fun EmptyBottles(
             color = BottlesTheme.color.text.primary
         )
         Spacer(modifier = Modifier.height(height = BottlesTheme.spacing.extraLarge))
-        CoilImage(
+        Image(
             modifier = Modifier.sizeIn(
                 minHeight = 250.dp,
                 minWidth = 250.dp,
                 maxHeight = 500.dp,
                 maxWidth = 500.dp,
             ),
-            imageModel = { image },
-            previewPlaceholder = painterResource(id = R.drawable.illustration_basket)
+            painter = painterResource(id = image),
+            contentDescription = null
         )
     }
 }
