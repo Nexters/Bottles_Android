ffb7ed10c0ad8ce56cbf7af9b99783a85a6939bc
ham2174 <ham121985@gmail.com>
2024-08-22 15:52:52 +0900

    [refactor] : BottlesTopBar Modifier 파라미터 변경

--- core/design-system/src/main/kotlin/com/team/bottles/core/designsystem/components/bars/TopBar.kt
+++ core/design-system/src/main/kotlin/com/team/bottles/core/designsystem/components/bars/TopBar.kt
@@ -1,6 +1,5 @@
 package com.team.bottles.core.designsystem.components.bars
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,7 +13,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,14 +27,14 @@ fun BottlesTopBar(
     trailingIcon: (@Composable () -> Unit)? = null,
 ) {
     Box(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth()
             .height(48.dp)
             .padding(horizontal = 16.dp),
         contentAlignment = Alignment.Center,
     ) {
         Row(
-            modifier = modifier.fillMaxWidth(),
+            modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = if (leadingIcon == null) Arrangement.End else Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
