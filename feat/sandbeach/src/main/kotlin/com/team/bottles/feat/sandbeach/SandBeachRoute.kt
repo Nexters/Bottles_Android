a63582e30117eea053d7ea943a566862c5f2337f
ham2174 <ham121985@gmail.com>
2024-08-24 03:36:57 +0900

    [feat] : 모래사장 화면에서 알림 권한 획득 구현

--- feat/sandbeach/src/main/kotlin/com/team/bottles/feat/sandbeach/SandBeachRoute.kt
+++ feat/sandbeach/src/main/kotlin/com/team/bottles/feat/sandbeach/SandBeachRoute.kt
@@ -1,12 +1,23 @@
 package com.team.bottles.feat.sandbeach
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.team.bottles.feat.sandbeach.mvi.SandBeachSideEffect
 
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
 @Composable
 internal fun SandBeachRoute(
     viewModel: SandBeachViewModel = hiltViewModel(),
@@ -15,6 +26,26 @@ internal fun SandBeachRoute(
     navigateToBottleBox: () -> Unit,
 ) {
     val uiState by viewModel.state.collectAsStateWithLifecycle()
+    val context = LocalContext.current
+    val notificationPermissionGranted =
+        remember {
+            ContextCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED
+        }
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            Toast.makeText(context, "동의했음", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(context, "동의안함", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    LaunchedEffect(notificationPermissionGranted) {
+        if (!notificationPermissionGranted) {
+            permissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+        }
+    }
 
     LaunchedEffect(Unit) {
         viewModel.sideEffect.collect { sideEffect ->
