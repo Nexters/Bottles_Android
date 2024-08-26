9ffbe51801c8f742b396cc874237a037be58ccf9
ham2174 <ham121985@gmail.com>
2024-08-27 02:44:59 +0900

    [build] : app 모듈 - datastore 모듈 의존성 제거

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,6 @@ dependencies {
     implementation(projects.core.designSystem)
     implementation(projects.core.data)
     implementation(projects.core.domain)
-    implementation(projects.core.datastore)
     implementation(projects.core.ui)
     implementation(projects.core.navigator)
     implementation(projects.core.common)
