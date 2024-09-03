b0000354107f76a7fbf4deb53cd21b952910fe65
ham2174 <ham121985@gmail.com>
2024-09-03 18:24:32 +0900

    [feat] : 디자인시스템 - shape L 사이즈 추가
    (cherry picked from commit b7c18963e8f7f1b4604d75b3975696c74a5e6ee6)

--- core/design-system/src/main/kotlin/com/team/bottles/core/designsystem/foundation/Shape.kt
+++ core/design-system/src/main/kotlin/com/team/bottles/core/designsystem/foundation/Shape.kt
@@ -8,6 +8,7 @@ internal enum class BottlesShapeDefaults(val shape: Shape) {
     RADIUS_XS(shape = RoundedCornerShape(8.dp)),
     RADIUS_S(shape = RoundedCornerShape(12.dp)),
     RADIUS_M(shape = RoundedCornerShape(16.dp)),
+    RADIUS_L(shape = RoundedCornerShape(20.dp)),
     RADIUS_XL(shape = RoundedCornerShape(24.dp)),
     ;
 }
