9ac0ae617a3e6bb1c37bc7c64a4e4ac93394d711
ham2174 <ham121985@gmail.com>
2024-09-23 18:16:02 +0900

    [chore] : 사용 안하는 웹뷰 URL 제거

--- feat/login/build.gradle.kts
+++ feat/login/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.konan.properties.Properties
-
 plugins {
     id("team.bottles.android.library.compose")
     id("team.bottles.android.library")
@@ -10,26 +8,6 @@ plugins {
 
 android {
     namespace = "com.team.bottles.feat.login"
-
-    buildTypes {
-        val debugUrl = "BOTTLES_SIGNIN_URL"
-        val properties = Properties().apply { load(rootProject.file("local.properties").inputStream()) }
-
-        getByName("release") {
-            buildConfigField(
-                "String",
-                debugUrl, // TODO : 릴리즈 용 URL 생성 가능성 있음
-                properties.getProperty(debugUrl) // // TODO : 릴리즈 용 URL 생성 가능성 있음
-            )
-        }
-        getByName("debug") {
-            buildConfigField(
-                "String",
-                debugUrl,
-                properties.getProperty(debugUrl)
-            )
-        }
-    }
 }
 
 dependencies {
