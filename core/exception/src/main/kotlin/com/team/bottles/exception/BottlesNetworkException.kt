c119ad548d932641a239d542b8942759d156e153
ham2174 <ham121985@gmail.com>
2024-09-25 03:03:58 +0900

    [feat] : BottlesException 및 BottlesNetworkException 정의

--- core/exception/src/main/kotlin/com/team/bottles/exception/BottlesNetworkException.kt
+++ core/exception/src/main/kotlin/com/team/bottles/exception/BottlesNetworkException.kt
@@ -1,43 +1,67 @@
 package com.team.bottles.exception
 
-import com.team.bottles.exception.BottlesNetworkException.Companion.INTERNAL_SERVER_ERROR_CODE
-import com.team.bottles.exception.BottlesNetworkException.Companion.INTERNAL_SERVER_ERROR_MESSAGE
-import com.team.bottles.exception.BottlesNetworkException.Companion.UNKNOWN_ERROR
-import com.team.bottles.exception.BottlesNetworkException.Companion.UNKNOWN_ERROR_MESSAGE
+import com.team.bottles.exception.BottlesNetworkException.Companion.CONNECTION_EXCEPTION_CODE
+import com.team.bottles.exception.BottlesNetworkException.Companion.CONNECTION_EXCEPTION_MESSAGE
+import com.team.bottles.exception.BottlesNetworkException.Companion.NO_ROUTE_TO_HOST_EXCEPTION_CODE
+import com.team.bottles.exception.BottlesNetworkException.Companion.NO_ROUTE_TO_HOST_EXCEPTION_MESSAGE
+import com.team.bottles.exception.BottlesNetworkException.Companion.SOCKET_EXCEPTION_CODE
+import com.team.bottles.exception.BottlesNetworkException.Companion.SOCKET_EXCEPTION_MESSAGE
+import com.team.bottles.exception.BottlesNetworkException.Companion.SOCKET_TIMEOUT_EXCEPTION_CODE
+import com.team.bottles.exception.BottlesNetworkException.Companion.SOCKET_TIMEOUT_EXCEPTION_MESSAGE
+import com.team.bottles.exception.BottlesNetworkException.Companion.UNKNOWN_HOST_EXCEPTION_CODE
+import com.team.bottles.exception.BottlesNetworkException.Companion.UNKNOWN_HOST_EXCEPTION_MESSAGE
+import java.io.IOException
+import java.net.ConnectException
+import java.net.NoRouteToHostException
 import java.net.SocketException
 import java.net.SocketTimeoutException
 import java.net.UnknownHostException
 
-class BottlesNetworkException(
-    code: Int,
+open class BottlesNetworkException(
+    val code: Int,
     override val message: String?,
-) : BottlesException(code, message) {
-    companion object {
-        const val INTERNAL_SERVER_ERROR_CODE = 500
-        const val INTERNAL_SERVER_ERROR_MESSAGE = "네트워크 연결을 확인할 수 없습니다. 잠시 후 다시 시도해주세요."
+) : IOException(message) {
 
-        const val UNKNOWN_ERROR = -9999
-        const val UNKNOWN_ERROR_MESSAGE = "알수 없는 에러가 발생하였습니다."
+    companion object {
+        const val SOCKET_TIMEOUT_EXCEPTION_CODE = 1001
+        const val CONNECTION_EXCEPTION_CODE = 1002
+        const val NO_ROUTE_TO_HOST_EXCEPTION_CODE = 1003
+        const val SOCKET_EXCEPTION_CODE = 1004
+        const val UNKNOWN_HOST_EXCEPTION_CODE = 1005
+
+        const val SOCKET_TIMEOUT_EXCEPTION_MESSAGE = "서버 응답이 지연되고 있습니다. 잠시 후 다시 시도해주세요."
+        const val CONNECTION_EXCEPTION_MESSAGE = "서버에 연결할 수 없습니다. 인터넷 연결을 확인해주세요."
+        const val NO_ROUTE_TO_HOST_EXCEPTION_MESSAGE = "잘못된 접근 입니다."
+        const val SOCKET_EXCEPTION_MESSAGE = "인터넷 연결이 끊겼습니다. 연결을 확인하고 다시 시도해주세요."
+        const val UNKNOWN_HOST_EXCEPTION_MESSAGE = "인터넷 연결이 끊겼습니다. 연결을 확인하고 다시 시도해주세요."
     }
 }
 
-fun Exception.parseWithNetworkError(): BottlesNetworkException {
-    return when (this) {
-        is UnknownHostException,
-        is SocketException,
-        is SocketTimeoutException,
-        -> {
-            BottlesNetworkException(
-                code = INTERNAL_SERVER_ERROR_CODE,
-                message = INTERNAL_SERVER_ERROR_MESSAGE,
-            )
-        }
+fun Exception.parseWithNetworkError(): Exception {
+    val exceptionMapping = mapOf(
+        SocketTimeoutException::class to BottlesNetworkException(
+            code = SOCKET_TIMEOUT_EXCEPTION_CODE,
+            message = SOCKET_TIMEOUT_EXCEPTION_MESSAGE
+        ),
+        ConnectException::class to BottlesNetworkException(
+            code = CONNECTION_EXCEPTION_CODE,
+            message = CONNECTION_EXCEPTION_MESSAGE
+        ),
+        NoRouteToHostException::class to BottlesNetworkException(
+            code = NO_ROUTE_TO_HOST_EXCEPTION_CODE,
+            message = NO_ROUTE_TO_HOST_EXCEPTION_MESSAGE
+        ),
+        SocketException::class to BottlesNetworkException(
+            code = SOCKET_EXCEPTION_CODE,
+            message = SOCKET_EXCEPTION_MESSAGE
+        ),
+        UnknownHostException::class to BottlesNetworkException(
+            code = UNKNOWN_HOST_EXCEPTION_CODE,
+            message = UNKNOWN_HOST_EXCEPTION_MESSAGE
+        )
+    )
 
-        else -> {
-            BottlesNetworkException(
-                code = UNKNOWN_ERROR,
-                message = UNKNOWN_ERROR_MESSAGE,
-            )
-        }
-    }
+    val mappedException = exceptionMapping[this::class] ?: this
+
+    return mappedException
 }
