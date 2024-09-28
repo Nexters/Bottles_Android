b27079ddc90b0e43983408c37785d219a00560b1
ham2174 <ham121985@gmail.com>
2024-09-28 16:04:31 +0900

    [feat] : 도착한 보틀 - 모래사장 애니메이션 구현

--- feat/bottle/src/main/kotlin/com/team/bottles/feat/bottle/navigation/BottleNavigation.kt
+++ feat/bottle/src/main/kotlin/com/team/bottles/feat/bottle/navigation/BottleNavigation.kt
@@ -2,6 +2,8 @@ package com.team.bottles.feat.bottle.navigation
 
 import ArrivedBottlesNavigator
 import MainNavigator
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
@@ -12,7 +14,20 @@ fun NavGraphBuilder.arrivedBottlesScreen(
     navigateToSandBeach: () -> Unit,
     navigateToBottleBox: () -> Unit
 ) {
-    composable<ArrivedBottlesNavigator> {
+    composable<ArrivedBottlesNavigator>(
+        exitTransition = {
+            slideOutOfContainer(
+                towards = AnimatedContentTransitionScope.SlideDirection.Right,
+                animationSpec = tween(300)
+            )
+        },
+        enterTransition = {
+            slideIntoContainer(
+                towards = AnimatedContentTransitionScope.SlideDirection.Left,
+                animationSpec = tween(300)
+            )
+        }
+    ) {
         ArrivedBottlesRoute(
             navigateToSandBeach = navigateToSandBeach,
             navigateToBottleBox = navigateToBottleBox
