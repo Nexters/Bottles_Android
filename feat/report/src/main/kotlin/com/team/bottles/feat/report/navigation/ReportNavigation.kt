7782c75401b0071ce72566ace472ca6a5d4d584b
ham2174 <ham121985@gmail.com>
2024-10-01 04:30:58 +0900

    [feat] : 핑퐁 - 신고 화면 이동 애니메이션 구현

--- feat/report/src/main/kotlin/com/team/bottles/feat/report/navigation/ReportNavigation.kt
+++ feat/report/src/main/kotlin/com/team/bottles/feat/report/navigation/ReportNavigation.kt
@@ -1,6 +1,8 @@
 package com.team.bottles.feat.report.navigation
 
 import ReportNavigator
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.team.bottles.feat.report.ReportRoute
@@ -9,7 +11,20 @@ fun NavGraphBuilder.reportScreen(
     navigateToPingPong: () -> Unit,
     navigateToBottleBox: () -> Unit,
 ) {
-    composable<ReportNavigator> {
+    composable<ReportNavigator>(
+        exitTransition = {
+            slideOutOfContainer(
+                towards = AnimatedContentTransitionScope.SlideDirection.Right,
+                animationSpec = tween(300)
+            )
+        },
+        enterTransition = {
+            slideIntoContainer(
+                towards = AnimatedContentTransitionScope.SlideDirection.Left,
+                animationSpec = tween(300)
+            )
+        }
+    ) {
         ReportRoute(
             navigateToPingPong = navigateToPingPong,
             navigateToBottleBox = navigateToBottleBox
